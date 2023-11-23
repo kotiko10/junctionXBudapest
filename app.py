05954657689ac0881dcf34d444881175cc576e10
Kotiko <koti1.nebieridze@gmail.com>
2023-11-23 22:38:24 +0100

    added: app.py  with fastapi and host names
    localhost working properly
    
    Ref: server

---
+++ app.py
@@ -0,0 +1,42 @@
+import uvicorn
+import subprocess
+from fastapi import FastAPI, Request
+import numpy as np
+import csv
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi import Form
+app = FastAPI(debug=True)
+templates = Jinja2Templates(directory='templates')
+
+@app.get('/', response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/Patients',response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/Floor_reservation',response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/Treatment_Reservation',response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/Patient_Stats',response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.get('/Machine_Stats',response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
