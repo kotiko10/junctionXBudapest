cbac712f326d89e49ad9fc141214691797fdca2c
Toma Sulaberidze <nikolonikoloa@gmail.com>
2023-11-25 04:57:27 +0100

    patient can not be added in occupied time

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -10,16 +10,28 @@ function calculateTopOffset(startTime) {
 function calculateHeight(duration) {
     return duration * 1; // Assuming 1 pixel per minute
 }
-  // Calculate the top offset in pixels
-// function calculateTopOffset(startTime) {
-//     const [hours, minutes] = startTime.split(':').map(Number);
-//     return ((hours - 8) * 60 + minutes) * 1; // 1 pixel per minute
-//   }
+
+function isTimeSlotAvailable(day, startTime, duration) {
+    const [startHours, startMinutes] = startTime.split(':').map(Number);
+    const startTotalMinutes = startHours * 60 + startMinutes;
+    const endTotalMinutes = startTotalMinutes + duration;
   
-  // Calculate the height of the appointment block in pixels
-function calculateHeight(duration) {
-    return duration * 1; // 1 pixel per minute
-}
+    // Find all existing appointments for the selected day
+    const existingAppointments = document.querySelectorAll(`.day-slot[data-day="${day}"] .appointment-block`);
+    for (let appointment of existingAppointments) {
+      const [appStartHours, appStartMinutes] = appointment.textContent.split(' - ')[0].split(':').map(Number);
+      const appDuration = parseInt(appointment.style.height, 10);
+      const appStartTotalMinutes = appStartHours * 60 + appStartMinutes;
+      const appEndTotalMinutes = appStartTotalMinutes + appDuration;
+  
+      // Check for overlap
+      if ((startTotalMinutes < appEndTotalMinutes) && (endTotalMinutes > appStartTotalMinutes)) {
+        return false; // Overlap found
+      }
+    }
+  
+    return true; // No overlap, time slot is available
+}  
   
 document.addEventListener('DOMContentLoaded', function () {
     // Handle time slot click
@@ -44,9 +56,15 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('addAppointmentForm').addEventListener('submit', function (event) {
         event.preventDefault();
         var time = this.elements['appointmentTime'].value;
-        console.log(time);
         var patientName = this.elements['patientName'].value;
         var duration = parseInt(this.elements['appointmentDuration'].value, 10);
+        var day = this.dataset.day;
+
+        // Check if the selected time slot is available
+        if (!isTimeSlotAvailable(day, time, duration)) {
+            alert('This time slot is already occupied. Please choose another time.');
+            return; // Prevent adding the appointment
+        }
         var topOffset = 0;
     
         var blockHeight = calculateHeight(duration);
@@ -54,10 +72,8 @@ document.addEventListener('DOMContentLoaded', function () {
         var appointmentBlock = document.createElement('div');
         appointmentBlock.className = 'appointment-block';
         appointmentBlock.style.top = topOffset + 'px';
-        console.log(topOffset);
         appointmentBlock.style.height = blockHeight + 'px';
         appointmentBlock.textContent = time + ' - ' + patientName;
-        console.log(this.dataset.hour);
         const [hours, minutes] = time.split(':').map(Number);
         var selectedCell = document.querySelector(`.day-slot[data-hour="${hours}"][data-day="${this.dataset.day}"] .appointment-container`);
         selectedCell.appendChild(appointmentBlock);
